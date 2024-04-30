2a16f1278a34e77f36e3636834a02f6cd5ad5643
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-30 12:21:38 -0700

    chat message model

--- src/main/java/com/zep/websocketchatapp/chat/ChatController.java
+++ src/main/java/com/zep/websocketchatapp/chat/ChatController.java
@@ -1,6 +1,10 @@
 package com.zep.websocketchatapp.chat;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
+    public  ChatMessage sendMessage(@Payload ChatMessage chatMessage){
+
+    }
 }
---
+++ src/main/java/com/zep/websocketchatapp/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.zep.websocketchatapp.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
