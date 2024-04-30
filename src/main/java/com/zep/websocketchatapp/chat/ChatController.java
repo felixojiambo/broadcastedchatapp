c469ed00bb4464c409e59814ad846ccb5fd568f1
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-30 12:34:31 -0700

    stomp header

--- src/main/java/com/zep/websocketchatapp/chat/ChatController.java
+++ src/main/java/com/zep/websocketchatapp/chat/ChatController.java
@@ -2,6 +2,7 @@ package com.zep.websocketchatapp.chat;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
@@ -13,7 +14,7 @@ return  chatMessage;
     }
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
-    public  ChatMessage addUser(@Payload ChatMessage chatMessage,SimpleMessageHeaderAccessor headerAccessor){
+    public  ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
         //add user to session
         headerAccessor.getSessionAttributes().put("username",chatMessage.getSender());
         return chatMessage;
--- src/main/java/com/zep/websocketchatapp/config/WebSocketEventListener.java
+++ src/main/java/com/zep/websocketchatapp/config/WebSocketEventListener.java
@@ -1,8 +1,10 @@
 package com.zep.websocketchatapp.config;
-
+import com.zep.websocketchatapp.chat.ChatMessage;
+import com.zep.websocketchatapp.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -15,6 +17,12 @@ public class WebSocketEventListener {
     public  void handleWebSocketDisconnectListener(
             SessionDisconnectEvent event
     ){
-
+        StompHeaderAccessor headerAccessor=StompHeaderAccessor.wrap(event.getMessage());
+        String username=(String) headerAccessor.getSessionAttributes().get("username");
+        if(username !=null){
+            log.info("User disconnected:{}",username);
+            var chatMessage= ChatMessage.builder().type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
     }
 }
