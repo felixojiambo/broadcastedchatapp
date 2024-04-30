b0e8d835d63d5d4038180fb77353582e6df6fc2d
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-30 12:27:45 -0700

    add user to session

--- src/main/java/com/zep/websocketchatapp/chat/ChatController.java
+++ src/main/java/com/zep/websocketchatapp/chat/ChatController.java
@@ -1,10 +1,21 @@
 package com.zep.websocketchatapp.chat;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
     public  ChatMessage sendMessage(@Payload ChatMessage chatMessage){
-
+return  chatMessage;
+    }
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public  ChatMessage addUser(@Payload ChatMessage chatMessage,SimpleMessageHeaderAccessor headerAccessor){
+        //add user to session
+        headerAccessor.getSessionAttributes().put("username",chatMessage.getSender());
+        return chatMessage;
     }
 }
