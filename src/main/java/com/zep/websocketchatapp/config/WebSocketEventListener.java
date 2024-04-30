eb2ff344c92e2522747d68fb006a6ae0e7e9c1bc
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-30 12:37:23 -0700

    alert user who left the chat other users//send to pulic topic

--- src/main/java/com/zep/websocketchatapp/config/WebSocketEventListener.java
+++ src/main/java/com/zep/websocketchatapp/config/WebSocketEventListener.java
@@ -4,6 +4,7 @@ import com.zep.websocketchatapp.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
@@ -13,16 +14,19 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 
 public class WebSocketEventListener {
+    private final SimpMessageSendingOperations messageTemplate;
     @EventListener
-    public  void handleWebSocketDisconnectListener(
+    public void handleWebSocketDisconnectListener(
             SessionDisconnectEvent event
-    ){
-        StompHeaderAccessor headerAccessor=StompHeaderAccessor.wrap(event.getMessage());
-        String username=(String) headerAccessor.getSessionAttributes().get("username");
-        if(username !=null){
-            log.info("User disconnected:{}",username);
-            var chatMessage= ChatMessage.builder().type(MessageType.LEAVE)
+    ) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected:{}", username);
+            var chatMessage = ChatMessage.builder().type(MessageType.LEAVE)
                     .sender(username)
                     .build();
+            messageTemplate.convertAndSend("/topic/public",chatMessage);
+        }
     }
 }
