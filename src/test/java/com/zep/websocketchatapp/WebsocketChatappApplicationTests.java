2c599d2bda2fb9758b7b1d01d701e7021bed25af
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-30 11:58:35 -0700

    setup

---
+++ src/test/java/com/zep/websocketchatapp/WebsocketChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zep.websocketchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketChatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
